9202fc8de6359937182132c10aa78e90826ed773
joshuawenata <joshuawenata@gmail.com>
2022-09-05 16:53:14 +0700

    welcome page + redirect button

---
+++ app/src/main/java/com/example/hackathon2022/WelcomePage.java
@@ -0,0 +1,24 @@
+package com.example.hackathon2022;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class WelcomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_page);
+    }
+
+    public void intoRegisterPage(View view){
+        startActivity(new Intent(this, RegisterPage.class));
+    }
+    public void intoLoginPage(View view){
+        startActivity(new Intent(this, LoginPage.class));
+    }
+
+}
