f1e3a06892d96401695ffe973cebe3b9e1103b3a
heryandjaruma <heryandjaruma@gmail.com>
2022-09-25 01:07:30 +0700

    additional pages added

---
+++ app/src/main/java/com/example/hackathon2022/TopSupplier.java
@@ -0,0 +1,14 @@
+package com.example.hackathon2022;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TopSupplier extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_supplier);
+    }
+}
