740502621032ab93b9c72f3c980ee470d881e375
mariauleria <mariaauleria304@gmail.com>
2022-10-03 22:32:22 +0700

    added login [ERROR]

--- app/src/main/java/com/example/hackathon2022/data/UserRepository.java
+++ app/src/main/java/com/example/hackathon2022/data/UserRepository.java
@@ -1,13 +1,22 @@
 package com.example.hackathon2022.data;
 
+import androidx.annotation.NonNull;
+
 import com.example.hackathon2022.data.models.User;
+import com.example.hackathon2022.interfaces.FinisihListener;
 import com.example.hackathon2022.utils.Crypt;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class UserRepository {
     private static FirebaseDatabase db = FirebaseDatabase.getInstance();
     private static DatabaseReference userRef = db.getReference("users");
+
+    public static User LOGGED_IN_USER = null;
 
     public static void insertUser(String userName, String userNomor, String userPass){
         String userKey = userRef.push().getKey();
@@ -16,7 +25,61 @@ public class UserRepository {
     }
 
     //TO DO: authentication user sign in
-    public static void auth(String nomor, String password){
+    public static void auth(String nomor, String password, FinisihListener<User> listener){
+        Query query =userRef.orderByChild("nomor").equalTo(nomor).limitToFirst(1);
 
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User temp = transformSnapshot(snapshot);
+
+                if(temp == null){
+                    //handle error
+                    if(listener != null) listener.onFinish(null, "Nomor tidak terdaftar!");
+                }
+                else{
+                    if(!Crypt.verifyHash(password, temp.getUserPass())){
+                        if(listener != null) listener.onFinish(null, "Password salah!");
+                    }
+                    else{
+                        if(listener != null) listener.onFinish(temp, "OK");
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    public static User transformSnapshot(DataSnapshot snapshot) {
+        if(!snapshot.exists()) return null;
+
+        String nomor, nama, password;
+        snapshot.getChildren().iterator().next();
+
+        nomor = snapshot.child("nomor").getValue(String.class);
+        nama = snapshot.child("nama").getValue(String.class);
+        password = snapshot.child("password").getValue(String.class);
+
+        return new User(snapshot.getKey(), nama, nomor, password);
+    }
+
+    public static void find(String userKey, FinisihListener<User> listener){
+        Query query =userRef.orderByKey().equalTo(userKey).limitToFirst(1);
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User temp = transformSnapshot(snapshot);
+                if(listener != null) listener.onFinish(temp, null);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
