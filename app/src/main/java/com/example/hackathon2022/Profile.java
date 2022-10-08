2c07412e3c51e1d2f10fb5ed2de13e16f790bc16
ASUS <joshuawenata@gmail.com>
2022-10-08 22:42:39 +0700

    benerin profile page, direct2

--- app/src/main/java/com/example/hackathon2022/Profile.java
+++ app/src/main/java/com/example/hackathon2022/Profile.java
@@ -2,7 +2,9 @@ package com.example.hackathon2022;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Profile extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+    }
+
+    public void intoSupplier(View view) {
+        startActivity(new Intent(this, SupplierPage.class));
+    }
+
+    public void intoLowongan(View view) {
+        startActivity(new Intent(this, LowonganPekerjaan.class));
+    }
+
+    public void intoHome(View view) {
+        startActivity(new Intent(this, HomePage.class));
     }
 }
