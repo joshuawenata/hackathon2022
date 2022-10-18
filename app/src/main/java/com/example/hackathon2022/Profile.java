e46a62d21b7b9a953529e2f950120430d7056642
ASUS <joshuawenata@gmail.com>
2022-10-18 15:50:49 +0700

    ubah profile

--- app/src/main/java/com/example/hackathon2022/Profile.java
+++ app/src/main/java/com/example/hackathon2022/Profile.java
@@ -2,22 +2,47 @@ package com.example.hackathon2022;
 
 import static com.example.hackathon2022.data.UserRepository.LOGGED_IN_USER;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.hackathon2022.Object.ObjectUser;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
+import java.io.IOException;
 
 public class Profile extends AppCompatActivity {
 
-    private EditText name;
-    private EditText phone;
+    private TextView name, phone, lokasi;
     private ImageButton btnLogout;
+    private ImageView imageavatar;
+
+    String namestr, nomorstr, lokasistr, deskripsistr, pathstr, kategoristr;
+
+    StorageReference storageReference;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +50,6 @@ public class Profile extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
         initComponents();
-        name.setText(LOGGED_IN_USER.getUserName());
-        phone.setText(LOGGED_IN_USER.getUserNomor());
 
         btnLogout.setOnClickListener(v -> {
             SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(Profile.this);
@@ -45,7 +68,48 @@ public class Profile extends AppCompatActivity {
     private void initComponents() {
         name = findViewById(R.id.activityprofile_name);
         phone = findViewById(R.id.activityprofile_phone);
+        lokasi = findViewById(R.id.activityprofile_lokasi);
+        imageavatar = findViewById(R.id.activityprofile_avatar);
         btnLogout = findViewById(R.id.logout_btn);
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("users");
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot postSnapshot : snapshot.getChildren()) {
+                    if(postSnapshot.getValue(ObjectUser.class).getKey().equals(LOGGED_IN_USER.getUserKey())){
+                        namestr = postSnapshot.getValue(ObjectUser.class).getNama();
+                        nomorstr = postSnapshot.getValue(ObjectUser.class).getNomor();
+                        lokasistr = postSnapshot.getValue(ObjectUser.class).getLokasi();
+                        deskripsistr = postSnapshot.getValue(ObjectUser.class).getDeskripsi();
+                        pathstr = postSnapshot.getValue(ObjectUser.class).getImagePath();
+                        kategoristr = postSnapshot.getValue(ObjectUser.class).getKategori();
+
+                        name.setText(namestr);
+                        phone.setText(nomorstr);
+                        lokasi.setText(lokasistr);
+                        storageReference = FirebaseStorage.getInstance().getReferenceFromUrl("gs://hackathon2022-85c99.appspot.com").child(pathstr);
+                        try {
+                            File localfile = File.createTempFile("temp","jpg");
+                            storageReference.getFile(localfile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                                @Override
+                                public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                                    Bitmap bitmap = BitmapFactory.decodeFile(localfile.getAbsolutePath());
+                                    imageavatar.setImageBitmap(bitmap);
+                                }
+                            });
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError firebaseError) {
+            }
+        });
     }
 
     public void intoSupplier(View view) {
@@ -64,8 +128,15 @@ public class Profile extends AppCompatActivity {
     }
 
     public void editProfile(View view) {
-        LOGGED_IN_USER.setUserName(name.getText().toString());
-        LOGGED_IN_USER.setUserNomor(phone.getText().toString());
+        Intent i = new Intent(this, UbahProfileActivity.class);
+        i.putExtra("name",namestr);
+        i.putExtra("phone",nomorstr);
+        i.putExtra("kategori",kategoristr);
+        i.putExtra("lokasi",lokasistr);
+        i.putExtra("deskripsi",deskripsistr);
+        i.putExtra("key",LOGGED_IN_USER.getUserKey());
+        i.putExtra("imagepath", pathstr);
+        startActivity(i);
     }
 
 }
